3f04dca246cfd44a90a0f155bd6a5d124eda274d
Juan Sebastián Loaiza Ramírez <61656777+Sebastian-LRZ@users.noreply.github.com>
2023-09-03 16:57:09 -0500

    Update consulta.py
    Se borraron comentarios innecesarios.

--- API/consulta.py
+++ API/consulta.py
@@ -1,8 +1,5 @@
 from sodapy import Socrata
 
-#  Unauthenticated client only works with public data sets. Note 'None'
-#  in place of application token, and no username or password:
-
 
 def obtener_cliente():
     # obtener accesso a los datos publicos en 'www.datos.gov.co'
