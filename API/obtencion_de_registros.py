2e8a124560fa2c17c35bba3c95e6b76056bb21d0
Juan Sebastián Loaiza Ramírez <sebastian.loaiza1@utp.edu.co>
2023-09-04 14:56:21 -0500

    Version Final

--- API/obtencion_de_registros.py
+++ API/obtencion_de_registros.py
@@ -11,7 +11,11 @@ def obtener_registros(departamento_a_consultar=None,
 
     results = consulta.obtener_cliente().get("ch4u-f3i5",
                                              limit=cantidad_de_registros,
-                                             select="departamento,municipio, cultivo, topografia, ph_agua_suelo_2_5_1_0, f_sforo_p_bray_ii_mg_kg, potasio_k_intercambiable_cmol_kg",
+                                             select="departamento, municipio, "
+                                                    "cultivo, topografia, "
+                                                    "ph_agua_suelo_2_5_1_0, "
+                                                    "f_sforo_p_bray_ii_mg_kg, "
+                                                    "potasio_k_intercambiable_cmol_kg",
                                              departamento=departamento_a_consultar,
                                              municipio=municipio_a_consultar,
                                              cultivo=cultivo_a_consultar)
